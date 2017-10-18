83837e3748bc59635fb5c068e805c723be6c25e6
Zbysław Kowalski <rooch@o2.pl>
2017-10-18 17:37:02 +0200

    Add Source Code and Test files

---
+++ EquilibriumChecker.Library/EquilibriumPointChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquilibriumPoint.Library
+{
+	public class EquilibriumPointChecker
+	{
+		public static int GetNumberOfEquilibriumPoints(int[,] input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ EquilibriumCheckerTests.Library/EquilibriumPointCheckerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquilibriumPoint.Library.Tests
+{
+	[TestFixture]
+    public class EquilibriumPointCheckerTests
+    {
+		[Test]
+		public void EquilibriumPointChecker_WhenNoElements_Returns0()
+		{
+			//Arrange
+			int[,] input = new int[0, 0];
+			//Act
+			int output = EquilibriumPointChecker.GetNumberOfEquilibriumPoints(input);
+			//Assert
+			Assert.AreEqual(0, output);
+		}
+    }
+}
